f402e9a46c7706d71b44d522ea73095295d59c3e
B401 <B401@DESKTOP-480FC2O>
2020-12-30 18:09:33 +0100

    ww

--- DBConnection1/Pages/SignInBase.cs
+++ DBConnection1/Pages/SignInBase.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using DBConnection1.Models;
+using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,16 +11,18 @@ namespace DBConnection1.Pages
     public class SignInBase : ComponentBase
     {
         protected string Day { get; set; } = DateTime.Now.DayOfWeek.ToString();
-        protected string User { get; set; }
+
+        [Required(ErrorMessage = "user name is required")]
+        protected string Username { get; set; }
         protected string Password{ get; set; }
         protected void UserNameChange(ChangeEventArgs eventArgs)
         {
-            User = eventArgs.Value.ToString();
+            Username = eventArgs.Value.ToString();
         }
 
         protected void UserNameValueChanged(string Value)
         {
-            User = Value;
+            Username = Value;
         }
     }
 }
